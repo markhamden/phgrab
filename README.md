88acaac5fb4d085b110d3c1ea75de08c14c7bd80
mark hamden <markhamden@users.noreply.github.com>
2015-02-16 17:36:45 +0100

    Made some minor improvements/updates. Also moved main from the global namespace to a method, improvements will be made at a later date.

--- README.md
+++ README.md
@@ -1,11 +1,9 @@
 # phgrab
-a commandline pornhost.com video grabber
+a commandline pornhost.com video grabber (v1.1)
 
 phgrab.py is a python script to download pornhost.com clips from the commandline.  
 It's made for/with Python3, requires BeautifulSoup4 and can use a http proxy.
 
-Enjoy!
-
-If you like my work (or even if you don't) please consider making a donation.  
+Please consider making a donation.
 Thank you!  
 Mark -- 1A6YkGKYy2bBTN56cad339sW7AxTNpFgch
--- phgrab.py
+++ phgrab.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-''' phgrab.py: pornhost.com clip downloader. '''
+''' phgrab.py: pornhost.com clip downloader. v1.1'''
 
 import os
 import sys
@@ -16,37 +16,44 @@ def reporthook(blocknum, blocksize, totalsize):
         percent = readsofar * 1e2 / totalsize
         display = "\r%*d / %d %5.0f%%" % (
             len(str(totalsize)), readsofar, totalsize, percent)
-        sys.stdout.write(display)
-        if readsofar >= totalsize:
-            sys.stdout.write("\n")
+        print(display, end='')
     else:   # total size unknown
-        sys.stderr.write("read %d\n" % (readsofar,))
+        print("read %d\n" % (readsofar,), end='')
 
 
-PARSER = argparse.ArgumentParser(
-    description="a commandline pornhost.com clip downloader.")
-PARSER.add_argument('clipcode', nargs='+',
-                    help="a ten-digit pornhost.com clipcode.")
-PARSER.add_argument('-p', '--proxy',
-                    help="A http proxy to use; e.g. http://localhost:8000")
+def main():
+    ''' Main method, to be improved at a later date. '''
+    print("phgrab.py v1.1; Please consider making a donation: 1A6YkGKYy2bBTN56cad339sW7AxTNpFgch\n")
 
-PROXY = PARSER.parse_args().proxy
-CLIPCODES = PARSER.parse_args().clipcode
+    parser = argparse.ArgumentParser(
+        description="a commandline pornhost.com clip downloader. v1.1")
+    parser.add_argument('clipcode', nargs='+',
+                        help="a ten-digit pornhost.com clipcode.")
+    parser.add_argument('-p', '--proxy',
+                        help="A http proxy to use; e.g. http://localhost:8000")
 
-if PROXY:
-    print("Grabbing through http proxy: %s\n" % PROXY)
-    os.environ['http_proxy'] = PROXY
+    proxy = parser.parse_args().proxy
+    clipcodes = parser.parse_args().clipcode
 
-for i in CLIPCODES:
-    if not i.isdigit() or not len(i) == 10:
-        sys.exit("'\033[1m%s\033[0m' is not a valid ten-digit clipcode, please check your input and try again." % i)
+    if proxy:
+        print("Grabbing through http proxy: %s\n" % proxy)
+        os.environ['http_proxy'] = proxy
 
-for i in CLIPCODES:
-    soup = bs4.BeautifulSoup(
-        urllib.request.urlopen("http://pornhost.com/%s" % i))
-    title = soup.find("h1", class_="video-title").string
-    href = soup.find("a", class_="download button")['href']
-    print("Grabbing \033[1m%s\033[0m:" % i)
-    local_filename, headers = urllib.request.urlretrieve(
-        href, '%s - %s.mp4' % (i, title), reporthook)
-    print("\033[1m", local_filename, "Done\033[0m")
+    for clipcode in clipcodes:
+        if not clipcode.isdigit() or len(clipcode) != 10:
+            print(
+                "'\033[1m%s\033[0m' is not a valid ten-digit clipcode, please check your input and try again." % clipcode)
+            sys.exit(1)
+
+    for clipcode in clipcodes:
+        soup = bs4.BeautifulSoup(
+            urllib.request.urlopen("http://pornhost.com/%s" % clipcode))
+        title = soup.find("h1", class_="video-title").string
+        href = soup.find("a", class_="download button")['href']
+        print("Grabbing \033[1m%s - %s.mp4\033[0m:" % (clipcode, title))
+        local_filename, headers = urllib.request.urlretrieve(
+            href, '%s - %s.mp4' % (clipcode, title), reporthook)
+        print("\033[1m Done\033[0m\n")
+
+if __name__ == "__main__":
+    main()
